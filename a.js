4e50b80ce70ae7db200470f3dc4bd536996b566d
mikey.zhaopeng <mikey.zhao@ndpmedia.com>
2016-06-07 15:04:49 +0800

    add tree

---
+++ a.js
@@ -0,0 +1,35 @@
+var fs = require('fs'),
+    util = require('util'),
+    path = './';
+
+function explorer(path) {
+
+    fs.readdir(path, function (err, files) {
+        //err 为错误 , files 文件名列表包含文件夹与文件
+        if (err) {
+            console.log('error:\n' + err);
+            return;
+        }
+
+        files.forEach(function (file) {
+
+            fs.stat(path + '/' + file, function (err, stat) {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                if (stat.isDirectory()) {
+                    // 如果是文件夹遍历
+                    explorer(path + '/' + file);
+                } else {
+                    // 读出所有的文件
+                    console.log('文件名:' + path + '/' + file);
+                }
+            });
+
+        });
+
+    });
+}
+
+explorer(path);
---
+++ src/js/note.js
@@ -0,0 +1,8 @@
+/*
+* @Author: mikey.zhaopeng
+* @Date:   2016-06-07 14:57:16
+* @Last Modified by:   mikey.zhaopeng
+* @Last Modified time: 2016-06-07 14:57:18
+*/
+
+'use strict';
